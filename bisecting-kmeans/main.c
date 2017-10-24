8368b79905f7244523c8285e39e7f353a81eb808
mariot <mariotjimenez@gmail.com>
2017-10-23 20:57:13 -0500

    Hello World

---
+++ bisecting-kmeans/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  bisecting-kmeans
+//
+//  Created by Mario Jimenez on 10/23/17.
+//  Copyright © 2017 mariot. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+    
+    
+    return 0;
+}
