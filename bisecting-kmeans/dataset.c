c50b7a6dead5e1ee3cd6dc2747fb33c530974443
mariot <mariotjimenez@gmail.com>
2017-10-30 14:52:41 -0500

    Added cluster functions

--- bisecting-kmeans/dataset.c
+++ bisecting-kmeans/dataset.c
@@ -22,3 +22,18 @@ void generateElements(int totalCoordinates, double* dataset) {
 double* getElementAtIndex(int dim, int elementIndex, double* data) {
     return &data[dim*elementIndex]; // [dim * elementIndex]: will give the starting index of the element requested
 }
+
+// Returns an element from the cluster_centroid at specified index to the dataElement argument
+void getClusterCentroidElement(int dim, int cluster, double* dataElement, double** cluster_centroid) {
+    for (int i = 0; i < dim; i++) {
+        dataElement[i] = cluster_centroid[cluster][i];
+    }
+}
+
+// Set a size dim data point to a specific cluster of the cluster_centroid
+void setClusterCentroid(int dim, int cluster, double* data, double** cluster_centroid) {
+    for (int i = 0; i < dim; i++) {
+        cluster_centroid[cluster][i] = data[i];
+    }
+}
+
