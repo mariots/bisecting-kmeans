926e3dc44c93be8f194432cf27e7ad7e71bdf064
mariot <mariotjimenez@gmail.com>
2017-10-30 14:51:24 -0500

    Initalizing all variables to describe the dataset

--- bisecting-kmeans/main.c
+++ bisecting-kmeans/main.c
@@ -1,18 +1,68 @@
 //
 //  main.c
-//  bisecting-kmeans
+//  bisecting-clustersmeans
 //
 //  Created by Mario Jimenez on 10/23/17.
 //  Copyright © 2017 mariot. All rights reserved.
 //
 
 #include <stdio.h>
+#include "dataset.h"
+#include "bisecting-kmeans.h"
+#include "debug.h"
 
-#define dim         2           // Number of Dimensions
-#define elements    24          // Number of Elements
-#define clusters    3           // Number of Clusters
+#define dim             2           // Number of Dimensions
+#define elements        24          // Number of Elements
+#define clusters        4           // Number of Clusters
 
 int main(int argc, const char * argv[]) {
+    
+    /*
+         Setup Variables
+     */
+    
+    int totalCoordinates = elements*dim;    // Total number of coordinates (x, y, z, ..., n)
+    double data[elements * dim];            // Number of data points
+    
+    int cluster_size[clusters] = { elements };  // Size of each clusters cluster
+    int cluster_start[clusters];                // Array start of each clusters cluster
+    
+    double** cluster_boundary;
+    double** cluster_centroid;              // For each cluster, have a mean for each dimension
+    double cluster_radius[clusters];
+    int cluster_assign[elements];           // Assigns each element (note an element is elements*dim values) to a cluster, 0...clusters clusters
+    
+    // set clusters and cluster_start to 0
+    for (int i = 0; i < clusters; i++) {
+        cluster_start[i] = 0;
+    }
+    
+    // set cluster_assign to all 0s
+    for (int i = 0; i < elements; i++) {
+        cluster_assign[i] = 0;
+    }
+    
+    // init cluster_boundary
+    cluster_boundary = (double **)calloc(clusters, sizeof(double *));
+    
+    for (int i = 0; i < clusters; i++) {
+        cluster_boundary[i] = (double *)calloc(dim * 2, sizeof(double));
+    }
+    
+    // init cluster_centroid
+    cluster_centroid = (double **)calloc(clusters, sizeof(double *));
+    
+    for (int i = 0; i < clusters; i++) {
+        cluster_centroid[i] = (double *)calloc(dim, sizeof(double));
+    }
+    
+    /*
+         Finished Setup
+     */
+    
+    generateElements(totalCoordinates, data);
+    
+    
     
     
     
