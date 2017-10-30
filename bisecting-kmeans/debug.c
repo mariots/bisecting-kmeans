f36ecb7add70e901544541ba0d975fccffaa5858
mariot <mariotjimenez@gmail.com>
2017-10-30 14:07:31 -0500

    Created debug functions

--- bisecting-kmeans/debug.c
+++ bisecting-kmeans/debug.c
@@ -7,3 +7,60 @@
 //
 
 #include "debug.h"
+
+/* debug functions */
+void printSpecificDataPoint(double *data, int requestedElement, int dim) {
+    printf("Element %d\n", requestedElement);
+    for (int i = 0; i < dim; i++) {
+        printf("array[%d]: %f\n", (dim * requestedElement) + i, data[(dim * requestedElement) + i]);
+    }
+}
+
+void printData(double *data, int totalElements, int dim) {
+    for (int i = 0; i < totalElements; i++) {
+        if(i % dim == 0) {
+            printf("Element %d\n", i / dim);
+        }
+        printf("array[%d]: %f\n", i, data[i]);
+    }
+}
+
+void printBoundry(double **cluster_bdry, int k, int dim) {
+    for (int i = 0; i < k; i++) {
+        for (int j = 0; j < dim*2; j++) {
+            printf("cluster_bdry[%d][%d]: %f\n", i, j, cluster_bdry[i][j]);
+        }
+    }
+}
+
+void printCentroid(double **cluster_centroid, int k, int dim) {
+    for (int i = 0; i < k; i++) {
+        for (int j = 0; j < dim; j++) {
+            printf("cluster_centroid[%d][%d]: %f\n", i, j, cluster_centroid[i][j]);
+        }
+    }
+}
+
+void printClusterSize(int *clusterSize, int k) {
+    for (int i = 0; i < k; i++) {
+        printf("cluster_size[%d]: %d\n", i, clusterSize[i]);
+    }
+}
+
+void printClusterStart(int *clusterStart, int k) {
+    for (int i = 0; i < k; i++) {
+        printf("cluster_start[%d]: %d\n", i, clusterStart[i]);
+    }
+}
+
+void printClusterAssign(int *clusterAssign, int ndata) {
+    for (int i = 0; i < ndata; i++) {
+        printf("cluster_assign[%d]: %d\n", i, clusterAssign[i]);
+    }
+}
+
+void printClusterRadius(double *clusterRadius, int k) {
+    for (int i = 0; i < k; i++) {
+        printf("cluster_radius[%d]: %f\n", i, clusterRadius[i]);
+    }
+}
--- bisecting-kmeans/debug.h
+++ bisecting-kmeans/debug.h
@@ -11,4 +11,14 @@
 
 #include <stdio.h>
 
+/* debug functions */
+void printSpecificDataPoint(double *data, int requestedElement, int dim);
+void printData(double *data, int totalElements, int dim);
+void printBoundry(double **cluster_bdry, int k, int dim);
+void printCentroid(double **cluster_centroid, int k, int dim);
+void printClusterSize(int *clusterSize, int k);
+void printClusterStart(int *clusterStart, int k);
+void printClusterAssign(int *clusterAssign, int ndata);
+void printClusterRadius(double *clusterRadius, int k);
+
 #endif /* debug_h */
